c2e6cfc55f2ea7b4431528b947404c9ca46e67a5
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-10 06:58:40 +0530

    Access Elements of an Array

---
+++ arrays.js
@@ -0,0 +1,16 @@
+const myArray = ['h', 'e', 'l', 'l', 'o'];
+
+// first element
+console.log(myArray[0]);  // "h"
+
+// second element
+console.log(myArray[1]); // "e"
+
+let Cities = ['Delhi','Mumbai','Banglore'];
+
+console.log(Cities[0]);
+console.log(Cities[1]);
+console.log(Cities[2]);
+//if we want to access out of index,it will show undefined
+console.log(Cities[3]);
+console.log(Cities[4]);
