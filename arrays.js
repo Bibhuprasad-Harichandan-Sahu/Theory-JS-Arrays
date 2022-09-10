9c5d1021a5e9b771919f9e3e0efc55ab938f6d3b
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-10 13:22:07 +0530

    Defining array of 2 types

--- arrays.js
+++ arrays.js
@@ -1,16 +1,8 @@
-const myArray = ['h', 'e', 'l', 'l', 'o'];
+//2 types we can create an array
 
-// first element
-console.log(myArray[0]);  // "h"
+let cars = ["civic","sonet","astor"]
+console.log(cars);//[ 'civic', 'sonet', 'astor' ]
+let bikes= new Array('rapid','octavia','slavia');
+console.log(bikes);//[ 'rapid', 'octavia', 'slavia' ]
 
-// second element
-console.log(myArray[1]); // "e"
 
-let Cities = ['Delhi','Mumbai','Banglore'];
-
-console.log(Cities[0]);
-console.log(Cities[1]);
-console.log(Cities[2]);
-//if we want to access out of index,it will show undefined
-console.log(Cities[3]);
-console.log(Cities[4]);
