831e33201c0a2ffaf0152a5001d8b3fd6a483d22
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-10 07:26:46 +0530

    Change the Elements of an Array

---
+++ 3.Change the Elements of an Array.js
@@ -0,0 +1,10 @@
+let cars = ["mercedez","BMW"];
+// this will add the new element 'Tesla' at the 2 index
+cars[2] = "Tesla";
+console.log(cars);
+// this will replace the value BMW to 'Hyundai' at the 1 index
+cars[1] = "hyundai";
+console.log(cars);
+// it will add Honda in the 5th index & 3,4 indexes will show undefined
+cars[5] = "Honda";
+console.log(cars);
