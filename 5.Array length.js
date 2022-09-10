2bf618f6b3a254e553de0f223039262e0e62e58a
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-10 07:49:30 +0530

    Array length

---
+++ 5.Array length.js
@@ -0,0 +1,16 @@
+const dailyActivities = [ 'eat', 'sleep'];
+
+// this gives the total number of elements in an array
+console.log(dailyActivities.length); // 2
+
+
+let Alphabets = ['a','b','c','d','e'];
+console.log(Alphabets.length);//length=5
+
+
+let movies = ['Ironman','captain america','Thor','Hulk']
+
+console.log(movies.length);//length =4
+
+
+
