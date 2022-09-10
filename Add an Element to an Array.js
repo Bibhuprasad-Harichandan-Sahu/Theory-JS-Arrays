a43d26e2e8bd7c3d04fd1ffe27cf6cd394a3b46d
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-09-10 07:12:14 +0530

    Add an Element to an Array

---
+++ Add an Element to an Array.js
@@ -0,0 +1,9 @@
+let people = ['mohit','sambit'];
+people.push('sameer');
+//push'll add an eelement at the End
+console.log(people);
+
+let mypeople = ['mohit','sambit'];
+////unSift'll add an element at the start
+mypeople.unshift('sameer');
+console.log(mypeople);
