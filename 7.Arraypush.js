d33fed19ce640a62f23d5cc706d05f2ad37cb3ba
Bibhuprasad Harichandan Sahu <bibhuprasad035@gmail.com>
2022-10-13 17:04:15 +0530

    arraypush

---
+++ 7.Arraypush.js
@@ -0,0 +1,6 @@
+let Cars = ["Vitara","Nexon","Tigun","Seltos"];
+Cars.push("Lamborghini");
+//[ 'Vitara', 'Nexon', 'Tigun', 'Seltos', 'Lamborghini' ]
+
+Cars.unshift("Lamborghini");
+console.log(Cars);
